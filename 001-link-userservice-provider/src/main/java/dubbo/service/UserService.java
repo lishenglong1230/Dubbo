072e3cd8c9c73c31311762ba34f1ed37ae7023fd
lsl <1215288388@qq.com>
2022-01-09 00:12:55 +0800

    link-userservice-provider

---
+++ 001-link-userservice-provider/src/main/java/dubbo/service/UserService.java
@@ -0,0 +1,8 @@
+package dubbo.service;
+
+import dubbo.model.User;
+
+public interface UserService {
+    User queryUserById(Integer id);
+
+}
---
+++ 001-link-userservice-provider/src/main/java/dubbo/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package dubbo.service.impl;
+
+import dubbo.model.User;
+import dubbo.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    public User queryUserById(Integer id) {
+        User user = new User();
+        user.setId(id);
+        user.setUsername("lisi");
+        user.setAge(23);
+        return user;
+    }
+}
