2c174bf5f0deafcdb7af612d34d39159a6143c50
lsl <1215288388@qq.com>
2022-01-17 13:12:46 +0800

    link-consumer

---
+++ 005-link-consumer/src/main/java/dubbo/web/UserController.java
@@ -0,0 +1,31 @@
+package dubbo.web;
+
+import dubbo.model.User;
+import dubbo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/userDetail")
+    public String userDetail(Model model,Integer id) {
+
+        //根据用户标识获取用户详情
+        User user = userService.queryUserById(id);
+
+        //获取用户总人数
+        Integer allUserCount = userService.queryAllUserCount();
+
+        model.addAttribute("user",user);
+        model.addAttribute("allUserCount",allUserCount);
+
+        return "userDetail";
+    }
+}
