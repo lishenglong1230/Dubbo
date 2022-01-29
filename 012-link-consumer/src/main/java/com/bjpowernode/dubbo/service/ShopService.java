857b602f4588ce9d8541a78f466072dd99efd084
lsl <1215288388@qq.com>
2022-01-29 18:01:28 +0800

    link-consumer

---
+++ 012-link-consumer/src/main/java/com/bjpowernode/dubbo/service/ShopService.java
@@ -0,0 +1,18 @@
+package com.bjpowernode.dubbo.service;
+
+/**
+ * ClassName:ShopService
+ * Package:com.bjpowernode.dubbo.service
+ * Description:
+ *
+ * @date:2020/3/9 12:07
+ * @author:蛙课网
+ */
+public interface ShopService {
+
+    /**
+     * hello
+     * @return
+     */
+    String hello();
+}
